d500ea1491af2963e1b172449e6966b4951d616d
Jack Kinsella <jack.kinsella@gmail.com>
2011-12-25 23:44:21 +0000

    reanmed sentencescans

--- lib/style/sentence_scans/excess_white_space_scan.rb
+++
@@ -1,9 +0,0 @@
-module Style
-  module SentenceScans
-    class ExcessWhiteSpace < Base 
-      def scan
-        create_alert suggested_sentence if sentence.gsub!(/\s+/, " ")
-      end
-    end
-  end
-end
