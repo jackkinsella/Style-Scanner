58699b23f28d75efd5327cc83dd8ee7a8bfefdb0
Jack Kinsella <jack.kinsella@gmail.com>
2011-12-27 12:42:37 +0000

    user friendly readout specs passing

--- lib/style/problems/base.rb
+++ lib/style/problems/base.rb
@@ -1,14 +1,19 @@
 module Style
   module Problems
     class Base
-      attr_reader :offending_text
+      attr_reader :offending_text, :sentence
 
-      def initialize(offending_text)
+      def initialize(sentece, offending_text)
+        @sentence = sentence
         @offending_text = offending_text
       end
 
       def on_text?(text)
         offending_text.strip_punctuation == text.strip_punctuation
+      end
+
+      def user_friendly_readout
+        [1,2,3].join(" | ")
       end
 
     end
--- spec/problems/base_spec.rb
+++ spec/problems/base_spec.rb
@@ -2,19 +2,37 @@ require "spec_helper"
 module Style
   module Problems 
     describe Base do
-      subject {Base.new("really")}
+      let(:sentence) {Sentence.new("I really like tomatoes.")}
+      subject {Base.new(sentence, "really")}
+
       it "knows its offending text" do
         subject.offending_text.should == "really"
       end
-      context "#on_text?" do
-        let(:very) {Base.new("very")}
-        it "true if problem is on that word" do
-          very.on_text?("very").should be_true
+
+      context "#user_friendly_readout" do
+        it "is delimited by \ so that cut command will work in linux" do
+          subject.user_friendly_readout.split("|").size.should == 3
         end
-        it "false if problem is on a different word" do
-          very.on_text?("really").should be_false
+        it "shows stringified problem name first" do
+          subject.user_friendly_readout.split("|")[0].should == "Useless Word"
+        end
+        it "shows problem sentence second" do
+          subject.user_friendly_readout.split("|")[1].should == "I really like tomatoes."
+        end
+        it "shows offending word third" do
+          subject.user_friendly_readout.split("|")[2].should == "really"
         end
       end
+
+      context "#on_text?" do
+        it "true if problem is on that word" do
+          subject.on_text?("really").should be_true
+        end
+        it "false if problem is on a different word" do
+          subject.on_text?("very").should be_false
+        end
+      end
+
     end
   end
 end
