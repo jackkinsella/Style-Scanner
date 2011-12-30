9ff7be158b9799cfbc30521a76a2e6e78744400b
Jack Kinsella <jack.kinsella@gmail.com>
2011-12-30 15:29:23 +0000

    results spec passes

--- spec/results_spec.rb
+++ spec/results_spec.rb
@@ -16,7 +16,7 @@ module Style
         Results.user_friendly_readout(sentences).split("\n").size.should == 1
       end
       it "contains the user_friendly_readout of the problem" do
-        Results.user_friendly_readout(sentences).match(problem.user_friendly_readout).should be_true
+        Results.user_friendly_readout(sentences).should == problem.user_friendly_readout
       end
     end
   end
