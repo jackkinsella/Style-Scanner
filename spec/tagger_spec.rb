5a373f6bede5cf887b037a5ba23b389be5d1b481
Jack Kinsella <jack.kinsella@gmail.com>
2012-01-25 20:06:50 +0100

    reformatted code in specs

--- spec/tagger_spec.rb
+++ spec/tagger_spec.rb
@@ -1,14 +1,14 @@
 require "spec_helper"
 module Style
-    describe Tagger do
-        let(:input) {"I am a wicked child."}
+  describe Tagger do
+    let(:input) {"I am a wicked child."}
 
-        context "#tagged_words" do
-            it "returns an array of tagged words object" do
-                tagged_words = Tagger.new(input).tagged_words
-                tagged_words.count.should == 6
-                tagged_words.first.kind_of?(TaggedWord).should be_true
-            end
-        end
+    context "#tagged_words" do
+      it "returns an array of tagged words object" do
+        tagged_words = Tagger.new(input).tagged_words
+        tagged_words.count.should == 6
+        tagged_words.first.kind_of?(TaggedWord).should be_true
+      end
     end
+  end
 end
