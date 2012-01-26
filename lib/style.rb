4cae0d7979572f10148aa01830b4759363a1cb18
Jack Kinsella <jack.kinsella@gmail.com>
2012-01-26 10:07:28 +0100

    removed some boilerplate

--- lib/style.rb
+++ lib/style.rb
@@ -8,12 +8,10 @@ require 'colorize'
 require "engtagger"
 require "sanitize"
 require 'redcloth'
+require 'trollop'
 
 require "style/problems/base"
 require "style/sentence_scans/base"
 Dir[File.dirname(__FILE__) + '/**/*.rb'].each do |file|
   require file
-end
-
-module Style
 end
