f6c4f2bcbed98b2d93ac925009cd66e366c1b888
Jack Kinsella <jack.kinsella@gmail.com>
2012-02-28 14:38:12 +0100

    improved ugly word readout

--- lib/style_scanner/sentence_scans/ugly_word.rb
+++ lib/style_scanner/sentence_scans/ugly_word.rb
@@ -7,7 +7,7 @@ module StyleScanner
 
       def scan
         WORD_PAIRS.keys.each do |offender|
-          create_problem(replacement_word(offender)) if sentence.contains?(offender) 
+          create_problem("replace #{offender} with #{replacement_word(offender)}") if sentence.contains?(offender) 
         end
       end
 
